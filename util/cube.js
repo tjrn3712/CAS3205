02042b933f6bbb72336b1c19976d8aae699f4754
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-10-10 16:01:09 +0900

    Update cube.js

--- util/cube.js
+++ util/cube.js
@@ -64,6 +64,108 @@ class Cube
 8) Fragment shader: should catch the vertex color from the vertex shader
 -----------------------------------------------------------------------------*/
 
+export class Pyramid {
+    constructor(gl, options = {}) {
+        this.gl = gl;
+        this.vao = gl.createVertexArray();
+        this.vbo = gl.createBuffer();
+        this.ebo = gl.createBuffer();
+
+        this.vertices = new Float32Array([
+            1.0,  1.0, 0,   // v0
+           -1.0,  1.0, 0,   // v1
+           -1.0, -1.0, 0,   // v2
+            1.0, -1.0, 0,   // v3
+            // 꼭짓점 (z=1)
+            0.0,  0.0, 1.0  // v4
+        ]);
+
+
+        this.normals = new Float32Array([
+            0, 0, -1,  0, 0, -1,  0, 0, -1,  0, 0, -1,
+            0, 1, 1,   0, 1, 1,   0, 1, 1,
+           -1, 0, 1,  -1, 0, 1,  -1, 0, 1,
+            0, -1, 1,   0, -1, 1,   0, -1, 1,
+            1, 0, 1,   1, 0, 1,   1, 0, 1
+        ]);
+
+
+        this.colors = new Float32Array([
+            1, 1, 0, 1,  1, 1, 0, 1,  1, 1, 0, 1,  1, 1, 0, 1,
+            1, 0, 0, 1,  1, 0, 0, 1,  1, 0, 0, 1,
+            0, 1, 0, 1,  0, 1, 0, 1,  0, 1, 0, 1,
+            0, 0, 1, 1,  0, 0, 1, 1,  0, 0, 1, 1,
+            1, 0, 1, 1,  1, 0, 1, 1,  1, 0, 1, 1
+        ]);
+
+        this.texCoords = new Float32Array([
+            1, 1,  0, 1,  0, 0,  1, 0,
+            0.5, 1,  0, 0,  1, 0,
+            0.5, 1,  0, 0,  1, 0,
+            0.5, 1,  0, 0,  1, 0,
+            0.5, 1,  0, 0,  1, 0
+        ]);
+
+        this.indices = new Uint16Array([
+            0, 1, 2,   2, 3, 0,
+            0, 1, 4,
+            1, 2, 4,
+            2, 3, 4,
+            3, 0, 4
+        ]);
+
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        const gl = this.gl;
+        const vSize = this.vertices.byteLength;
+        const nSize = this.normals.byteLength;
+        const cSize = this.colors.byteLength;
+        const tSize = this.texCoords.byteLength;
+        const totalSize = vSize + nSize + cSize + tSize;
+
+        gl.bindVertexArray(this.vao);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, totalSize, gl.STATIC_DRAW);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.vertices);
+        gl.bufferSubData(gl.ARRAY_BUFFER, vSize, this.normals);
+        gl.bufferSubData(gl.ARRAY_BUFFER, vSize + nSize, this.colors);
+        gl.bufferSubData(gl.ARRAY_BUFFER, vSize + nSize + cSize, this.texCoords);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ebo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, vSize);
+        gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 0, vSize + nSize);
+        gl.vertexAttribPointer(3, 2, gl.FLOAT, false, 0, vSize + nSize + cSize);
+
+        gl.enableVertexAttribArray(0);
+        gl.enableVertexAttribArray(1);
+        gl.enableVertexAttribArray(2);
+        gl.enableVertexAttribArray(3);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        gl.bindVertexArray(null);
+    }
+
+    draw(shader) {
+        const gl = this.gl;
+        shader.use();
+        gl.bindVertexArray(this.vao);
+        gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_SHORT, 0);
+        gl.bindVertexArray(null);
+    }
+
+    delete() {
+        const gl = this.gl;
+        gl.deleteBuffer(this.vbo);
+        gl.deleteBuffer(this.ebo);
+        gl.deleteVertexArray(this.vao);
+    }
+}
+
 export class Cube {
     constructor(gl, options = {}) {
         this.gl = gl;
